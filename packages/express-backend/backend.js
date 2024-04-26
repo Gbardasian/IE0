53f7dc77d0982195143a531095e316f6c6dd4bf1
Gannon Bardasian <fbardasian@gmail.com>
2024-04-26 09:50:58 -0700

    Step 6 complete

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -52,6 +52,7 @@ app.get("/users", (req, res) => {
 			      }
 });
 
+
 const findUserById = (id) =>
 	  users["users_list"].find((user) => user["id"] === id);
 
@@ -64,6 +65,19 @@ app.get("/users/:id", (req, res) => {
 			        res.send(result);
 			      }
 });
+
+
+const addUser = (user) => {
+	  users["users_list"].push(user);
+	  return user;
+};
+
+app.post("/users", (req, res) => {
+	  const userToAdd = req.body;
+	  addUser(userToAdd);
+	  res.send();
+});
+
 
 app.listen(port, () => {
 	  console.log(
